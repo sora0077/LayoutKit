3b008f9083e26b3df5f31e29a736dd62c4b1e171
t.hayashi <t.hayashi0077+bitbucket@gmail.com>
2015-01-11 18:50:50 +0900

    Initial Commit

---
+++ LayoutKit/LayoutKit.h
@@ -0,0 +1,19 @@
+//
+//  LayoutKit.h
+//  LayoutKit
+//
+//  Created by 林 達也 on 2015/01/11.
+//  Copyright (c) 2015年 林 達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LayoutKit.
+FOUNDATION_EXPORT double LayoutKitVersionNumber;
+
+//! Project version string for LayoutKit.
+FOUNDATION_EXPORT const unsigned char LayoutKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LayoutKit/PublicHeader.h>
+
+
